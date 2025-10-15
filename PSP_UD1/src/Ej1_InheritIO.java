ff954f5b72b2fc85155232bac168958decf59d63
Ana Núñez <nunez.flores.ana@iescamas.es>
2025-10-15 21:41:08 +0200

    Clase InheritIO creada

---
+++ PSP_UD1/src/Ej1_InheritIO.java
@@ -0,0 +1,8 @@
+public class Ej1_InheritIO {
+    public static void main(String[] args) throws Exception {
+        int return_code = new ProcessBuilder(Utils.sh(String.join(" ", args)))
+                .inheritIO()
+                .start()
+                .waitFor();
+    }
+}
